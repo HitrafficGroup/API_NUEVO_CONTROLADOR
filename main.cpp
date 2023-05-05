d42d095de6dceb6ef253c3262ad0798f29e2d423
david <david.diaz190799@gmail.com>
2023-05-05 11:33:11 -0500

    se configura el repositorio

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <QCoreApplication>
 #include "myudp.h"
+#include "string"
 #include "variables.h"
+using namespace std;
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
+    string opcion = argv[2];
     MyUDP socket;
+    cout<<"hola mundo";
+    printf("esta es la opcion %s : \n",argv[2]);
+    if(opcion == "--get_fases"){
+            printf("obteniendo fases del controlador %s : \n",argv[1]);
 
+       }else if(opcion == "--get_secuencias"){
+            printf("obteniendo las secuencias del controlador %s\n :",argv[1]);
+       }
     //client.HelloUDP();
+    //socket.ReadData(O_CurrentTime);
+    //socket.ReadData(O_DeviceInfo);
 
-    socket.ReadData(O_CurrentTime);
-    socket.ReadData(O_DeviceInfo);
-    return a.exec();
 }
